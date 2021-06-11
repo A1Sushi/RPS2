8e8025482e668ec15e8e5047faacf6312279f009
Tommy Tran <tommytran99@outlook.com>
2021-06-11 16:08:24 -0400

    Add to README & clean up Code in RPS.JS

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Hello! this is my very first project that involves using HTML/CSS/JS Together!
 
 Summary
 - It is a game of Rock, Paper, Scissors that can be played in the console as well as the browser 
+- oldRPS.js runs only in the console 
+- RPS.JS runs on the browser
 
 Learned
 - Creating a problem solving plan before writing code 
@@ -14,3 +16,4 @@ Learned
 - Using callback functions
 - Using increments to keep score of 5 round game and declare winner at the end
 - Using loops to play through 5 rounds of a game
+- Adding UI through DOM Manipulation using event listeners
--- RPS.js
+++ RPS.js
@@ -12,8 +12,8 @@ function computerPlay() {
     // Display winner and loser each call using if...else statements *
     // Return "invalid choice" if selection is not part of choice *
     // Make playerSelection parameter case-insensitive *
-    // Display Scores in console (added console.log scores for every possibility)
-    // Add a div for displaying results and change all of your console.logs into DOM methods.
+    // Display Scores in console (added console.log scores for every possibility) 
+    // Add a div for displaying results and change all of your console.logs into DOM methods. *
 
 function playRound(playerSelection, computerSelection) {
     console.log(playerSelection)
@@ -165,50 +165,14 @@ function playRound(playerSelection, computerSelection) {
 
 let playerSelection = "";
 
-/* Test Results of Function Call 
-    let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-*/
-
 // Create function that plays a game 5x using the playRound function inside & keeps score each round declaring the winner at the end. 
-    // No interface played in the console
-    // Interaction = user picking a weapon (use prompt) 
-    // Desire output = Declare winner of the game after 5 rounds
-
-// when game() is called; 
-    // Play a round of RPS (use computerPlay because we want a random selection from computer each time the function is called)
-    // if player wins increment playerScore by 1 
-        // 1. Create 2 variable to store scores
-        // 2. Use increment operator to add score (might have to do this in playRound) 
-    // else (computer wins) increment computerScore by 1 
-    // Make sure scores are saved after each round 
-    // After 5 rounds return message declaring winner based on highest score!  
+    // Interface required to play through buttons
+    // Interaction = user presses button to choose weapon
+    // Desire output = Alert winner after someone reaches 5 points 
 
 // Variables to Hold Scores
 let playerScore = 0
 let computerScore = 0
-
-// Player Selection Choice - Event Listeners 
-// const btn = document.querySelector('#btn');
-// btn.addEventListener('click', () => {
-//   alert("You Picked Rock!");
-// });
-
-
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        playRound(prompt("Rock, Paper, or Scissors?").toLowerCase(), computerPlay());
-    }
-
-    if (playerScore > computerScore) {
-        return "Player Wins Game!!!";
-    } else if (playerScore < computerScore) {
-        return "Computer Wins Game :(";
-    } else {
-        return "Tie Game!";
-    }
-}
 
 // Creating Buttons for PlayerChoice 
 const buttons = document.querySelectorAll('button');
@@ -225,11 +189,7 @@ buttons.forEach((button) => {
     })
 })
 
-// Testing Single Round Game With Buttons 
-// console.log(game())
-//console.log(playRound(prompt("Rock, Paper, or Scissors?").toLowerCase();, computerPlay()));
-
-/* Currently @
+/* To Improve 
 1. Refractor code for playRound into smaller codes 
     - So, it looks like you have all that code repeated through all 9 if statements? If so, then yes that's not ideal. Save yourself some writing and make your function much more maintainable by eliminating the duplication. Don't be afraid to write extra functions.
     - You can definitely make things a lot easier on yourself by moving some of that conditional code into a function or multiple functions and simplifying your playRound function.
